39e1880dc3cc8a40edbfbcd2d61de97d3c8290de
Rwigenzadavy <r.niyoyande@alustudent.com>
2026-09-14 15:39:48 +0200

    feat: defining Member interface

---
+++ src/types/member.ts
@@ -0,0 +1,11 @@
+// Author: Rwigenza Niyoyandemye
+// Assigned tasks: 41–42, 44–46, 48–49
+
+export interface Member {
+  id: string;
+  name: string;
+  role: string;
+  tasksCompleted: number;
+  isActive: boolean;
+  bio?: string;
+}
